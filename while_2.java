56eeefedfc5a9b0253f6d9e9dbc681438196bf8d
likkib2005 <likkilikitha214@gmail.com>
2024-12-05 16:23:36 +0530

    Add files via upload

---
+++ while_2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class While_2
+{
+    public static void main(String args[])
+{
+    int n=100;
+    int i=n;
+    while(i>-1)
+    {
+        System.out.println(i);
+        i-=1;
+     }
+}
+}
